6b7dc8feb1a96df967d2bba724fa5177e0e25b1e
Sadan Mian <sadanmiyan007@gmail.com>
2024-09-29 21:04:42 +0530

    Project Organization

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,19 +1,9 @@
-import Counter from "../components/Counter";
+import Counter from "@/app/_components/Counter";
 
-export default async function Page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
-
-  console.log(data);
+export default function Page() {
   return (
     <div>
       <h1>Cabins Pageeee</h1>
-      <ul>
-        {data.map((e) => (
-          <li key={e?.id}>{e?.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
