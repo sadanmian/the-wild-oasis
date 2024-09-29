0cc3b59b41630de1c3c5b3a009bb61b1e7a3c5ba
Sadan Mian <sadanmiyan007@gmail.com>
2024-09-29 17:49:38 +0530

    Displaying a loading Indicator

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ import Navigation from "./components/Navigation";
 export default function Page() {
   return (
     <div>
-      {/* <Navigation /> */}
       <h1>The Wild Oasis. Welcome to paradise.</h1>
-
       <Link href={"/cabins"}>Explore Luxury Cabins</Link>
     </div>
   );
