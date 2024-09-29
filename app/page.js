6cfa920a139ed8136e0a966b8e6be58d71eeba9f
Sadan Mian <sadanmiyan007@gmail.com>
2024-09-29 10:09:07 +0530

    Navigating between pages

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
 export default function Page() {
-  return <h1>The Wild Oasis. Welcome to paradise.</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>The Wild Oasis. Welcome to paradise.</h1>
+
+      <Link href={"/cabins"}>Explore Luxury Cabins</Link>
+    </div>
+  );
 }
