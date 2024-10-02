98e9c51ca94613d9b822ca6e8ec1fb2eee96c5df
Sadan Mian <sadanmiyan007@gmail.com>
2024-10-02 17:14:00 +0530

    make dynamic page static with generateStaticParams and Caching

--- app/_components/CabinList.js
+++ app/_components/CabinList.js
@@ -1,7 +1,9 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabin, getCabins } from "../_lib/data-service";
+import { unstable_noStore } from "next/cache";
 
 async function CabinList() {
+  // unstable_noStore();
   const cabins = await getCabins();
 
   if (!cabins.length) {
