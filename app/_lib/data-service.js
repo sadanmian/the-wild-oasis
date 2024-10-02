b64eec76b10564e1e936d18a85056596110cc69f
Sadan Mian <sadanmiyan007@gmail.com>
2024-10-02 10:24:12 +0530

    Dynamic Route Segments

--- app/_lib/data-service.js
+++ app/_lib/data-service.js
@@ -39,7 +39,7 @@ export const getCabins = async function () {
     .order("name");
 
   // For testing
-  await new Promise((res) => setTimeout(res, 2000));
+  // await new Promise((res) => setTimeout(res, 2000));
 
   if (error) {
     console.error(error);
