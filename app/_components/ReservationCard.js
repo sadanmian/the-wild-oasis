09e0f5401bc7085c0ce3eba34e7419bcff9f5d36
Sadan Mian <sadanmiyan007@gmail.com>
2024-10-03 21:02:45 +0530

    Another Loading Indicator: The useTransition Hook

--- app/_components/ReservationCard.js
+++ app/_components/ReservationCard.js
@@ -80,10 +80,9 @@ function ReservationCard({ booking }) {
               <PencilSquareIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
               <span className="mt-1">Edit</span>
             </Link>
+            <DeleteReservation bookingId={id} />
           </>
         ) : null}
-
-        <DeleteReservation bookingId={id} />
       </div>
     </div>
   );
