8dafbf5002412e483f7b48dd1bae3c38567876f0
Sadan Mian <sadanmiyan007@gmail.com>
2024-10-03 10:46:41 +0530

    feat: create route.js

---
+++ app/api/cabins/[cabinId]/route.js
@@ -0,0 +1,15 @@
+import { getBookedDatesByCabinId, getCabin } from "@/app/_lib/data-service";
+
+export async function GET(request, { params }) {
+  const { cabinId } = params;
+
+  try {
+    const [cabin, bookedDate] = await Promise.all([
+      getCabin(cabinId),
+      getBookedDatesByCabinId(cabinId),
+    ]);
+    return Response.json({ cabin, bookedDate });
+  } catch (error) {
+    return Response.json({ message: "Cabin not found" });
+  }
+}
