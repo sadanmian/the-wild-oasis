9661d112d77a354451b5a967d662358970ae8940
Sadan Mian <sadanmiyan007@gmail.com>
2024-10-03 22:40:52 +0530

    Updating a Reservation

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import { updateGuest } from "../_lib/actions";
-import { useFormStatus } from "react-dom";
+import SubmitButton from "./SubmitButton";
 // import SelectCountry from "./SelectCountry";
 
 function UpdateProfileForm({ guest, children }) {
@@ -56,22 +56,9 @@ function UpdateProfileForm({ guest, children }) {
           className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
         />
       </div>
-      <Button />
+      <SubmitButton>Update Profile</SubmitButton>
       <div className="flex justify-end items-center gap-6"></div>
     </form>
-  );
-}
-
-function Button() {
-  const { pending } = useFormStatus();
-
-  return (
-    <button
-      disabled={pending}
-      className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
-    >
-      {pending ? "Updating..." : "Update profile"}
-    </button>
   );
 }
 
